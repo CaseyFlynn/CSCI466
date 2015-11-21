9d27e20dc19fe229c5f02b59e36af4aa13fe68b0
jodoesp <jespinosa@mtech.edu>
2015-11-20 18:34:48 -0700

    fixed warnings

--- Game/src/com/flynnovations/game/server/GameInfo.java
+++ Game/src/com/flynnovations/game/server/GameInfo.java
@@ -1,11 +1,10 @@
 package com.flynnovations.game.server;
 
 import java.io.Serializable;
-import java.util.*;
-
 import com.flynnovations.game.server.Game;
 
 public class GameInfo implements Serializable{
+	private static final long serialVersionUID = 1L;
 	public String gameName;
 	public int gameId;
 	public int numberOfPlayers;
